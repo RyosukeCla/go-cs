2c8ae6067fde6ba172219e514692600167553df0
RyosukeCla <ryosukeclarinet@gmail.com>
2019-10-02 18:45:14 +0900

    add huffman

--- pkg/huffman/huffman.go
+++ pkg/huffman/huffman.go
@@ -1,19 +1,61 @@
 package huffman
 
 import (
+	"sort"
+
 	"github.com/RyosukeCla/go-playground/pkg/bits"
 )
 
+// Symbol type aiueo
 type Symbol struct {
-	Symbol     []byte
+	Symbol     string
 	Occurrence uint64
 }
 
+// Code type aiueo
 type Code struct {
-	Symbol []byte
-	Code   *bits.BitStream
+	Symbol string
+	Code   []byte
+}
+
+// BuildCode returns bits
+func BuildCode(order int, maxOrder int) []byte {
+	if order == 0 {
+		return []byte{0}
+	} else if order == maxOrder {
+		len := order
+		codes := make([]byte, len)
+		for i := 0; i < len; i++ {
+			codes[i] = 1
+		}
+		return codes
+	} else {
+		len := order + 1
+		codes := make([]byte, len)
+		for i := 0; i < order; i++ {
+			codes[i] = 1
+		}
+		codes[len-1] = 0
+		return codes
+	}
 }
 
 // Build builds symbols
 func Build(symbols []Symbol) []Code {
+	symbolLen := len(symbols)
+	sortedSymbols := make([]Symbol, symbolLen)
+	copy(sortedSymbols, symbols)
+	sort.SliceStable(sortedSymbols, func(i, j int) bool { return symbols[i].Occurrence < symbols[j].Occurrence })
+
+	codes := make([]Code, symbolLen)
+	for i, symbol := range sortedSymbols {
+		bitStream := bits.NewBitStream()
+		bitStream.Write([]byte{0})
+		codes[i] = Code{
+			Symbol: symbol.Symbol,
+			Code:   BuildCode(i, symbolLen-1),
+		}
+	}
+
+	return codes
 }
--- pkg/huffman/huffman_test.go
+++ pkg/huffman/huffman_test.go
@@ -1,13 +1,19 @@
 package huffman
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestBuild(t *testing.T) {
-	Build([]Symbols{
-		{ Symbol: "a", Occurrence: 1 },
+	codes := Build([]Symbol{
+		{Symbol: "a", Occurrence: 1},
+		{Symbol: "b", Occurrence: 2},
+		{Symbol: "c", Occurrence: 3},
+		{Symbol: "d", Occurrence: 4},
 	})
+
+	fmt.Println(codes)
 	if 0 != 0 {
 		t.Fatal("aoiep")
 	}
