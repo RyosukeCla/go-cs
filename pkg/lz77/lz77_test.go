2be6a93df73ea536e0e7b8340014454bb09d8dd6
RyosukeCla <ryosukeclarinet@gmail.com>
2019-10-03 02:43:32 +0900

    fix

--- pkg/lz77/lz77_test.go
+++ pkg/lz77/lz77_test.go
@@ -41,17 +41,22 @@ func TestDecoder(t *testing.T) {
 }
 
 func TestEncoderAndDecoder(t *testing.T) {
-	input := []byte("lorem ipsum")
+	input := []byte(`
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	`)
 
 	encoder := NewEncoder(100)
 	encoder.Write(input)
-	codes := make([]Code, 1000)
+	codes := make([]Code, 500)
 	n, _ := encoder.Read(codes)
 	codes = codes[:n]
 
 	decoder := NewDecoder()
 	decoder.Write(codes)
-	output := make([]byte, 1000)
+	output := make([]byte, 500)
 	n, _ = decoder.Read(output)
 	output = output[:n]
 
