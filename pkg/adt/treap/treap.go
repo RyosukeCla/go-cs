c37e2bfabc520562ee68e775355fdbca2ddecbe0
RyosukeCla <ryosukeclarinet@gmail.com>
2019-10-14 20:57:17 +0900

    fifx

--- pkg/adt/treap/treap.go
+++ pkg/adt/treap/treap.go
@@ -24,6 +24,16 @@ func (t *Treap) Insert(key int) {
 	t.root = insert(t.root, key)
 }
 
+// Search searches noe
+func (t *Treap) Search(key int) bool {
+	return search(t.root, key)
+}
+
+// Remove removes node
+func (t *Treap) Remove(key int) {
+	remove(t.root, key)
+}
+
 func insert(root *node, key int) *node {
 	if root == nil {
 		return createNode(key)
@@ -60,6 +70,48 @@ func leftRotate(root *node) *node {
 	newRoot.left = root
 	root.right = tree
 	return newRoot
+}
+
+func search(parent *node, key int) bool {
+	if parent == nil {
+		return false
+	} else if parent.key == key {
+		return true
+	} else if parent.key < key {
+		return search(parent.right, key)
+	} else {
+		return search(parent.left, key)
+	}
+}
+
+func remove(parent *node, key int) *node {
+	if parent == nil {
+		return nil
+	}
+
+	if key < parent.key {
+		parent.left = remove(parent.left, key)
+	} else if parent.key < key {
+		parent.right = remove(parent.right, key)
+	} else if parent.left == nil {
+		newParent := parent.right
+		parent.right = nil
+		return newParent
+	} else if parent.right == nil {
+		newParent := parent.left
+		parent.left = nil
+		return newParent
+	} else if parent.left.value < parent.right.value {
+		newParent := leftRotate(parent)
+		newParent.left = remove(newParent.left, key)
+		return newParent
+	} else {
+		newParent := rightRotate(parent)
+		newParent.right = remove(newParent.right, key)
+		return newParent
+	}
+
+	return parent
 }
 
 func createNode(key int) *node {
--- pkg/adt/treap/treap_test.go
+++ pkg/adt/treap/treap_test.go
@@ -15,3 +15,19 @@ func TestTreap(t *testing.T) {
 	fmt.Println(treap)
 	fmt.Println(treap.root.key, treap.root.left.key, treap.root.right.key)
 }
+
+func TestTreap_Remove(t *testing.T) {
+	treap := NewTreap()
+	treap.Insert(1)
+	treap.Insert(2)
+	treap.Insert(3)
+	treap.Insert(4)
+	treap.Insert(5)
+	treap.Remove(3)
+	treap.Remove(2)
+
+	fmt.Println("addf-1")
+	fmt.Println("asdf-2", treap.Search(2), treap.Search(4))
+	fmt.Println("asdf-3", treap.Search(3), treap.Search(5))
+	fmt.Println("asdf-4", treap.Search(3), treap.Search(100))
+}
