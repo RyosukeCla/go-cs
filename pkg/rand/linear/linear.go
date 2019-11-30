a6939faaf11280ffc88e39fb4029e552e2040dfb
RyosukeCla <ryosukeclarinet@gmail.com>
2019-11-30 19:31:34 +0900

    add linear congruential method

---
+++ pkg/rand/linear/linear.go
@@ -0,0 +1,29 @@
+package linear
+
+// Rand is for linear congruential generation
+type Rand struct {
+	previous int
+}
+
+// NewRand returns Rand that implements linear congruential generation
+func NewRand(seed int) Rand {
+	return Rand{
+		previous: seed,
+	}
+}
+
+// MODULUS is 2^23 - 1
+const MODULUS = 4294967296 - 1
+
+// MULTIPLIER is 48271
+const MULTIPLIER = 48271
+
+// INCREMENT is 0
+const INCREMENT = 0
+
+// Generate generates rand: X_next = (MULTIPLIER * X_pre + INCREMENT) % MODULUS.
+func (r *Rand) Generate() int {
+	next := (MULTIPLIER*r.previous + INCREMENT) % MODULUS
+	r.previous = next
+	return next
+}
---
+++ pkg/rand/linear/linear_test.go
@@ -0,0 +1,12 @@
+package linear
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	rand := NewRand(123)
+	for i := 0; i < 1000; i++ {
+		rand.Generate()
+	}
+}
