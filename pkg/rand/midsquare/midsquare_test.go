86067102e15516595d18f80044d021dddf89526e
RyosukeCla <RyosukeCla@users.noreply.github.com>
2019-12-17 14:19:28 +0900

    fix

--- pkg/rand/midsquare/midsquare_test.go
+++ pkg/rand/midsquare/midsquare_test.go
@@ -3,12 +3,12 @@ package midsquare
 import (
 	"testing"
 
-	"github.com/RyosukeCla/go-cs/pkg/hashtable/basic"
+	"github.com/RyosukeCla/go-cs/pkg/hashtable/robinhood"
 )
 
 func Test(t *testing.T) {
 	N := 10
-	seen := basic.NewHashTable(uint(5050))
+	seen := robinhood.NewHashTable(uint(5000))
 	rand := NewRand(4, 9090)
 	for i := 0; i < N; i++ {
 		r := rand.Generate()
