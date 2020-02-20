29ccb1f29b7f277cb3b456d65023f9b9af1d8d56
RyosukeCla <RyosukeCla@users.noreply.github.com>
2020-02-20 18:03:39 +0900

    fux

--- pkg/mutex/spinlock/spinlock.go
+++ pkg/mutex/spinlock/spinlock.go
@@ -5,17 +5,17 @@ import (
 	"sync/atomic"
 )
 
-type Mutex struct {
+type mutex struct {
 	lock uint32
 }
 
-func New() Mutex {
-	return Mutex{
+func New() mutex {
+	return mutex{
 		lock: uint32(0),
 	}
 }
 
-func (m *Mutex) Lock() {
+func (m *mutex) Lock() {
 	for {
 		if atomic.CompareAndSwapUint32(&m.lock, 0, 1) {
 			break
@@ -24,6 +24,6 @@ func (m *Mutex) Lock() {
 	}
 }
 
-func (m *Mutex) Unlock() {
+func (m *mutex) Unlock() {
 	atomic.StoreUint32(&m.lock, 0)
 }
